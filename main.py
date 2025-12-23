5176b33d465dd3751b064653e2a64c40b83092d3
lomi <jeanremi.telmon@free.fr>
2025-12-23 16:27:04 +0100

    Test on linux

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import torch
+
+
 def main():
     print("Hello from edf!")
+    print(f"Torch version: {torch.__version__}")
+    print(f"Torch CUDA available: {torch.cuda.is_available()}")
 
 
 if __name__ == "__main__":
